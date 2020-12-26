25c69dd2c679b57bbb369c2f43c801acddecd149
Cameron Gutman <aicommander@gmail.com>
2020-12-26 13:38:37 -0600

    Fix accidental hardcoding of x64

--- build-openssl.sh
+++ build-openssl.sh
@@ -4,7 +4,7 @@ pushd ../openssl
 
 # Build OpenSSL
 mkdir ../build/openssl/build_$1
-./Configure --prefix=$PWD/../build/openssl/build_$1 --openssldir=$PWD/../build/openssl/build_x64 $2 -mmacosx-version-min=10.13
+./Configure --prefix=$PWD/../build/openssl/build_$1 --openssldir=$PWD/../build/openssl/build_$1 $2 -mmacosx-version-min=10.13
 make -j$(sysctl -n hw.ncpu)
 make install
 
