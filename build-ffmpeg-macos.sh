4c2815b5f2045ef0e8b098ed2fa8c4f9c1f5cd77
Cameron Gutman <aicommander@gmail.com>
2020-12-26 21:33:05 -0600

    Add patch to disable OpenGL compatibility in VT decoder output

--- build-ffmpeg-macos.sh
+++ build-ffmpeg-macos.sh
@@ -3,6 +3,9 @@ set -e
 
 pushd ../FFmpeg
 
+# Apply our FFmpeg patch for macOS
+git apply ../patches/ffmpeg_no_ogl_vt.patch
+
 # Build FFmpeg
 mkdir ../build/FFmpeg/build_$1
 ./configure --prefix=$PWD/../build/FFmpeg/build_$1 --extra-cflags="-mmacosx-version-min=10.13 -arch $2 --target=$2-apple-macosx" --extra-ldflags="-mmacosx-version-min=10.13 -arch $2" --cc=clang --arch=$2 --enable-cross-compile --install-name-dir=@loader_path/../Frameworks --enable-pic --enable-shared --disable-static --disable-all --enable-avcodec --enable-decoder=h264 --enable-decoder=hevc --enable-hwaccel=h264_videotoolbox --enable-hwaccel=hevc_videotoolbox
