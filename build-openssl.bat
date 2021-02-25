122c868da53796e19904e7ba5f18ef2fd65c34af
Cameron Gutman <aicommander@gmail.com>
2021-02-24 23:56:22 -0600

    Attempt to speed up OpenSSL build

--- build-openssl.bat
+++ build-openssl.bat
@@ -7,9 +7,9 @@ cd ..\openssl
 rem Build OpenSSL
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" %2
 mkdir ..\build\openssl\build_%1
-perl Configure --prefix=%CD%\..\build\openssl\build_%1 --openssldir=%CD%\..\build\openssl\build_%1 %3
+perl Configure --prefix=%CD%\..\build\openssl\build_%1 --openssldir=%CD%\..\build\openssl\build_%1 %3 no-tests
 nmake
-nmake install
+nmake install_sw
 
 rem Clean up in-tree build
 git reset --hard
