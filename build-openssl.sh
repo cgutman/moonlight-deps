19ca139ca95239557f5f1d6db1462f58c0e83cda
Cameron Gutman <aicommander@gmail.com>
2021-02-24 23:56:22 -0600

    Attempt to speed up OpenSSL build

--- build-openssl.sh
+++ build-openssl.sh
@@ -4,9 +4,9 @@ pushd ../openssl
 
 # Build OpenSSL
 mkdir ../build/openssl/build_$1
-./Configure --prefix=$PWD/../build/openssl/build_$1 --openssldir=$PWD/../build/openssl/build_$1 $2 -mmacosx-version-min=10.13
+./Configure --prefix=$PWD/../build/openssl/build_$1 --openssldir=$PWD/../build/openssl/build_$1 $2 -mmacosx-version-min=10.13 no-tests
 make -j$(sysctl -n hw.ncpu)
-make install
+make install_sw
 
 # Clean up in-tree build
 git reset --hard
