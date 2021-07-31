6ad9c0374de1f4aa3123098c75f4d0c6822fdafc
Cameron Gutman <aicommander@gmail.com>
2021-07-31 12:21:49 -0500

    Update SDL and set SDL_MAC_NO_SANDBOX=1 for keyboard grab

--- build-sdl.sh
+++ build-sdl.sh
@@ -5,5 +5,5 @@ set -e
 OUTPUT_DIR=`pwd`
 pushd ../../SDL/Xcode/SDL
 pwd
-xcodebuild -project SDL.xcodeproj -scheme Framework -configuration Release -sdk macosx CONFIGURATION_BUILD_DIR=$OUTPUT_DIR
+xcodebuild -project SDL.xcodeproj -scheme Framework -configuration Release -sdk macosx CONFIGURATION_BUILD_DIR=$OUTPUT_DIR OTHER_CFLAGS="-DSDL_MAC_NO_SANDBOX=1"
 popd
