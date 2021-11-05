6905a9c00cee71d724c5a086f226caff8e86b851
Cameron Gutman <aicommander@gmail.com>
2021-11-04 20:35:40 -0500

    Update SDL and change CMake variables to use new SDL prefix

--- build-sdl.bat
+++ build-sdl.bat
@@ -6,18 +6,18 @@ set LDFLAGS=/DEBUG
 
 mkdir build_x86
 cd build_x86
-cmake %CMAKE_ARGS% -DBUILD_SHARED_LIBS=ON -DLIBC=ON -A Win32 %REPO_PATH%
+cmake %CMAKE_ARGS% -DBUILD_SHARED_LIBS=ON -DSDL_LIBC=ON -A Win32 %REPO_PATH%
 cmake --build . --config Release -v
 cd ..
 
 mkdir build_x64
 cd build_x64
-cmake %CMAKE_ARGS% -DBUILD_SHARED_LIBS=ON -DLIBC=ON -A x64 %REPO_PATH%
+cmake %CMAKE_ARGS% -DBUILD_SHARED_LIBS=ON -DSDL_LIBC=ON -A x64 %REPO_PATH%
 cmake --build . --config Release -v
 cd ..
 
 mkdir build_arm64
 cd build_arm64
-cmake %CMAKE_ARGS% -DBUILD_SHARED_LIBS=ON -DLIBC=ON -A ARM64 %REPO_PATH%
+cmake %CMAKE_ARGS% -DBUILD_SHARED_LIBS=ON -DSDL_LIBC=ON -A ARM64 %REPO_PATH%
 cmake --build . --config Release -v
 cd ..
