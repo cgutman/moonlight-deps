08cfc7fe7f1f684d77c2739d47eeacdc972db9cc
Cameron Gutman <aicommander@gmail.com>
2022-08-09 21:14:37 -0500

    Revert "Reintroduce hardcoded compiler path to try to fix FFmpeg build"
    This reverts commit 53ee7fc5cf986475e641c9ac648f12d16b4f0105.

--- build-ffmpeg-windows.sh
+++ build-ffmpeg-windows.sh
@@ -1,4 +1,3 @@
-VCBASE="/c/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.32.31326/bin/Hostx64"
 GENERIC_BUILD_ARGS="--enable-pic --enable-shared --disable-static --disable-all --enable-avcodec --enable-decoder=h264 --enable-decoder=hevc --enable-hwaccel=h264_dxva2 --enable-hwaccel=hevc_dxva2 --enable-hwaccel=h264_d3d11va --enable-hwaccel=hevc_d3d11va --enable-hwaccel=h264_d3d11va2 --enable-hwaccel=hevc_d3d11va2"
 
 # Our MSYS command drops us in a random folder. Reorient ourselves based on this script directory.
@@ -12,20 +11,18 @@ if [ "$1" = "x86" ]; then
     pacman --noconfirm -S yasm
 
     TARGET_BUILD_ARGS="--arch=i686 --toolchain=msvc --enable-cross-compile"
-    export PATH="$VCBASE/x86":$PATH
 elif [ "$1" = "x64" ]; then
     # x64 uses yasm for assembly
     pacman --noconfirm -S yasm
 
     TARGET_BUILD_ARGS="--arch=x86_64 --toolchain=msvc"
-    export PATH="$VCBASE/x64":$PATH
 elif [ "$1" = "arm64" ]; then
     # ARM64 uses gas-preprocessor.pl for assembly
     mkdir /tmp/gas
     wget https://raw.githubusercontent.com/FFmpeg/gas-preprocessor/master/gas-preprocessor.pl -O /tmp/gas/gas-preprocessor.pl
 
     TARGET_BUILD_ARGS="--arch=arm64 --toolchain=msvc --enable-cross-compile"
-    export PATH="$VCBASE/arm64":/tmp/gas:$PATH
+    export PATH=/tmp/gas:$PATH
 fi
 
 mkdir $OUTDIR
