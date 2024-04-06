d0ba651a15e3946902a1abc5f2f1f5ae6b112ab8
Cameron Gutman <aicommander@gmail.com>
2024-04-06 09:35:16 -0500

    Fix FFmpeg library major version

--- archive.sh
+++ archive.sh
@@ -15,8 +15,8 @@ lipo build/opus/build_*/libopus.a -create -o output/lib/libopus.a
 lipo build/discord-rpc/build_*/src/libdiscord-rpc.a -create -o output/lib/libdiscord-rpc.a
 lipo build/openssl/build_*/lib/libssl.a -create -o output/lib/libssl.a
 lipo build/openssl/build_*/lib/libcrypto.a -create -o output/lib/libcrypto.a
-lipo build/FFmpeg/build_*/lib/libavcodec.60.dylib -create -o output/lib/libavcodec.60.dylib
-lipo build/FFmpeg/build_*/lib/libavutil.58.dylib -create -o output/lib/libavutil.58.dylib
+lipo build/FFmpeg/build_*/lib/libavcodec.61.dylib -create -o output/lib/libavcodec.61.dylib
+lipo build/FFmpeg/build_*/lib/libavutil.59.dylib -create -o output/lib/libavutil.59.dylib
 
 cp -R build/SDL/ output/Frameworks/
 
