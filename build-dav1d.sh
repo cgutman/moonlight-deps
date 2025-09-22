5fe5b9b9a2c0307a61e47e9c2e1cdd3fb224bb4b
Cameron Gutman <aicommander@gmail.com>
2025-09-21 20:55:24 -0500

    Enable cross-compilation of dav1d on Apple Silicon

--- build-dav1d.sh
+++ build-dav1d.sh
@@ -3,17 +3,30 @@ set -e
 
 COMMON_OPTIONS="-Ddefault_library=static -Dbuildtype=debugoptimized -Denable_tools=false -Denable_tests=false"
 
+# Enable cross compilation if needed
+arch=$(uname -m)
+if [[ "$arch" == "arm64" ]]; then
+  X64_OPTIONS="--cross-file=../../../meson-crossfiles/apple-arm64_amd64.txt"
+  STATIC_LIB_INSTALL_PATH="/opt/homebrew/lib/libdav1d.a"
+elif [[ "$arch" == "x86_64" ]]; then
+  ARM64_OPTIONS="--cross-file=../../../meson-crossfiles/apple-amd64_arm64.txt"
+  STATIC_LIB_INSTALL_PATH="/usr/local/lib/libdav1d.a"
+else
+  echo "Unknown architecture: $arch"
+  exit 1
+fi
+
 # Build dav1d for x64
 mkdir build_x64
 pushd build_x64
-CFLAGS="-arch x86_64 -mmacosx-version-min=$MACOS_MIN" LDFLAGS="-arch x86_64 -mmacosx-version-min=$MACOS_MIN" meson setup ../../../dav1d $COMMON_OPTIONS
+CFLAGS="-arch x86_64 -mmacosx-version-min=$MACOS_MIN" LDFLAGS="-arch x86_64 -mmacosx-version-min=$MACOS_MIN" meson setup ../../../dav1d $X64_OPTIONS $COMMON_OPTIONS
 meson compile
 popd
 
 # Build dav1d for arm64
 mkdir build_arm64
 pushd build_arm64
-CFLAGS="-arch arm64 -mmacosx-version-min=$MACOS_MIN" LDFLAGS="-arch arm64 -mmacosx-version-min=$MACOS_MIN" meson setup ../../../dav1d --cross-file=../../../meson-crossfiles/apple-amd64_arm64.txt $COMMON_OPTIONS
+CFLAGS="-arch arm64 -mmacosx-version-min=$MACOS_MIN" LDFLAGS="-arch arm64 -mmacosx-version-min=$MACOS_MIN" meson setup ../../../dav1d $ARM64_OPTIONS $COMMON_OPTIONS
 meson compile
 popd
 
@@ -22,4 +35,4 @@ lipo build_x64/src/libdav1d.a build_arm64/src/libdav1d.a -create -o build_arm64/
 
 # Install the universal binary
 sudo ninja -C build_arm64 install
-lipo -info /usr/local/lib/libdav1d.a
+lipo -info $STATIC_LIB_INSTALL_PATH
