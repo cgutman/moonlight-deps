475c5019cf14ab4b229742e849d2a3ec9e7e3895
Cameron Gutman <aicommander@gmail.com>
2025-09-24 00:02:42 -0500

    Build libplacebo with clang-cl

--- build-libplacebo-windows.bat
+++ build-libplacebo-windows.bat
@@ -1,17 +1,14 @@
-rem ARM64 is not supported for cross-compilation
-if /I "%1"=="arm64" if /I not "%PROCESSOR_ARCHITECTURE%"=="ARM64" (
-    echo Cross-compiling libplacebo for ARM64 is not supported on x64
-    exit /b 0
-)
+rem Build using the CL frontend for Clang
+set CC=clang-cl
+set CXX=clang-cl
 
-rem Use the MSYS environment with the correct compiler toolchain
-set MSYSTEM=%2
+set OUTDIR=%CD%\..\build\libplacebo\build_%1
+mkdir %OUTDIR%
 
-rem Build the DLL
-cmd /c msys2 -c "../build-libplacebo-windows.sh %1"
+meson setup --prefix=%OUTDIR% -Ddefault_library=shared -Dbuildtype=debugoptimized -Ddemos=false -Dtests=false -Dopengl=disabled -Dd3d11=disabled -Dvulkan=enabled -Dvk-proc-addr=disabled -Dxxhash=disabled -Dshaderc=disabled build
+ninja -C build
+ninja -C build install
 
-rem Create the def file
-cmd /c msys2 -c "../create-libplacebo-def.sh %1"
-
-rem Create the import library
-lib.exe /machine:%1 /def:libplacebo\build_%1\bin\libplacebo.def /out:libplacebo\build_%1\bin\libplacebo.lib
+rem This build was in-tree, so clean it up
+git reset --hard
+git clean -f -d -x
