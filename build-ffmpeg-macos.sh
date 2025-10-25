a3a177e8d709c2ba72573697d1c085139203208f
Cameron Gutman <aicommander@gmail.com>
2025-10-25 13:55:32 -0500

    Disable autodetection of external libraries in FFmpeg build
    This can inadvertently link to Homebrew libraries present on the builder.

--- build-ffmpeg-macos.sh
+++ build-ffmpeg-macos.sh
@@ -8,7 +8,7 @@ git apply ../patches/ffmpeg_metal_vt.patch
 
 # Build FFmpeg
 mkdir ../build/FFmpeg/build_$1
-PKG_CONFIG_PATH="/usr/local/lib/pkgconfig" ./configure --prefix=$PWD/../build/FFmpeg/build_$1 --extra-cflags="-mmacosx-version-min=$MACOS_MIN -arch $2 --target=$2-apple-macosx" --extra-ldflags="-mmacosx-version-min=$MACOS_MIN -arch $2" --cc=clang --arch=$2 --enable-cross-compile --install-name-dir=@loader_path/../Frameworks --enable-pic --enable-shared --disable-static --disable-all --enable-avcodec --enable-swscale --enable-decoder=h264 --enable-decoder=hevc --enable-hwaccel=h264_videotoolbox --enable-hwaccel=hevc_videotoolbox --enable-hwaccel=av1_videotoolbox --enable-libdav1d --enable-decoder=libdav1d
+PKG_CONFIG_PATH="/usr/local/lib/pkgconfig" ./configure --prefix=$PWD/../build/FFmpeg/build_$1 --extra-cflags="-mmacosx-version-min=$MACOS_MIN -arch $2 --target=$2-apple-macosx" --extra-ldflags="-mmacosx-version-min=$MACOS_MIN -arch $2" --cc=clang --arch=$2 --enable-cross-compile --install-name-dir=@loader_path/../Frameworks --enable-pic --fatal-warnings --enable-shared --disable-static --disable-all --disable-autodetect --enable-avcodec --enable-swscale --enable-decoder=h264 --enable-decoder=hevc --enable-videotoolbox --enable-hwaccel=h264_videotoolbox --enable-hwaccel=hevc_videotoolbox --enable-hwaccel=av1_videotoolbox --enable-libdav1d --enable-decoder=libdav1d
 make -j$(sysctl -n hw.ncpu)
 make install
 
--- build-ffmpeg-windows.sh
+++ build-ffmpeg-windows.sh
@@ -1,4 +1,4 @@
-GENERIC_BUILD_ARGS="--enable-pic --enable-shared --disable-static --disable-all --enable-avcodec --enable-avformat --enable-swscale --enable-decoder=h264 --enable-decoder=hevc --enable-decoder=av1 --enable-hwaccel=h264_dxva2 --enable-hwaccel=hevc_dxva2 --enable-hwaccel=av1_dxva2 --enable-hwaccel=h264_d3d11va --enable-hwaccel=hevc_d3d11va --enable-hwaccel=av1_d3d11va --enable-hwaccel=h264_d3d11va2 --enable-hwaccel=hevc_d3d11va2 --enable-hwaccel=av1_d3d11va2 --enable-hwaccel=h264_d3d12va --enable-hwaccel=hevc_d3d12va --enable-hwaccel=av1_d3d12va --enable-libdav1d --enable-decoder=libdav1d --extra-cflags=-I$VULKAN_SDK\Include --enable-hwaccel=h264_vulkan --enable-hwaccel=hevc_vulkan --enable-hwaccel=av1_vulkan"
+GENERIC_BUILD_ARGS="--enable-pic --fatal-warnings --enable-shared --disable-static --disable-all --disable-autodetect --enable-dxva2 --enable-d3d11va --enable-d3d12va --enable-vulkan --enable-avcodec --enable-avformat --enable-swscale --enable-decoder=h264 --enable-decoder=hevc --enable-decoder=av1 --enable-hwaccel=h264_dxva2 --enable-hwaccel=hevc_dxva2 --enable-hwaccel=av1_dxva2 --enable-hwaccel=h264_d3d11va --enable-hwaccel=hevc_d3d11va --enable-hwaccel=av1_d3d11va --enable-hwaccel=h264_d3d11va2 --enable-hwaccel=hevc_d3d11va2 --enable-hwaccel=av1_d3d11va2 --enable-hwaccel=h264_d3d12va --enable-hwaccel=hevc_d3d12va --enable-hwaccel=av1_d3d12va --enable-libdav1d --enable-decoder=libdav1d --extra-cflags=-I$VULKAN_SDK\Include --enable-hwaccel=h264_vulkan --enable-hwaccel=hevc_vulkan --enable-hwaccel=av1_vulkan"
 
 # Our MSYS command drops us in a random folder. Reorient ourselves based on this script directory.
 SCRIPT=$(readlink -f $0)
@@ -10,9 +10,7 @@ cd $SCRIPTPATH/FFmpeg
 git apply ../patches/ffmpeg_dxva_hevc_rext.patch
 
 if [ "$1" = "x64" ]; then
-    # x64 uses yasm for assembly
-    pacman --noconfirm -S yasm
-
+    # x64 uses nasm for assembly
     TARGET_BUILD_ARGS="--arch=x86_64 --toolchain=msvc --extra-ldflags=-cetcompat"
 elif [ "$1" = "arm64" ]; then
     # ARM64 uses gas-preprocessor.pl for assembly
