1bdf8719fbea640a50d28029f86fbb1e81b8e4ed
Cameron Gutman <aicommander@gmail.com>
2026-05-26 21:18:19 -0500

    Build libopus and libdiscord-rpc as shared libraries for macOS
    This is necessary to avoid incompatibilities with LTO using different
    Clang versions.

--- archive.sh
+++ archive.sh
@@ -17,8 +17,8 @@ cp build/SDL/install/lib/libSDL3.dylib output/lib/libSDL3.dylib
 cp build/sdl2-compat/install/lib/libSDL2.dylib output/lib/libSDL2.dylib
 cp build/SDL_ttf/install/lib/libSDL2_ttf.dylib output/lib/libSDL2_ttf.dylib
 
-lipo build/opus/build_*/libopus.a -create -o output/lib/libopus.a
-lipo build/discord-rpc/build_*/src/libdiscord-rpc.a -create -o output/lib/libdiscord-rpc.a
+lipo build/opus/build_*/libopus.0.dylib -create -o output/lib/libopus.0.dylib
+lipo build/discord-rpc/build_*/src/libdiscord-rpc.dylib -create -o output/lib/libdiscord-rpc.dylib
 lipo build/openssl/build_*/lib/libssl.3.dylib -create -o output/lib/libssl.3.dylib
 lipo build/openssl/build_*/lib/libcrypto.3.dylib -create -o output/lib/libcrypto.3.dylib
 lipo build/FFmpeg/build_*/lib/libavcodec.62.dylib -create -o output/lib/libavcodec.62.dylib
--- build-opus.sh
+++ build-opus.sh
@@ -3,7 +3,8 @@ set -e
 
 mkdir build_$1
 cd build_$1
-cmake -DCMAKE_OSX_ARCHITECTURES=$2 \
+cmake -DOPUS_BUILD_SHARED_LIBRARY=ON \
+      -DCMAKE_OSX_ARCHITECTURES=$2 \
       -DCMAKE_OSX_DEPLOYMENT_TARGET=$MACOS_MIN \
       -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=ON \
       ../../../opus/
