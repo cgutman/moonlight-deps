c3c43315f842059c43dbc6108258fd74a068f478
Cameron Gutman <aicommander@gmail.com>
2026-07-26 20:20:59 -0500

    Build Detours with EHCont

--- build-detours.bat
+++ build-detours.bat
@@ -1,5 +1,13 @@
 set DETOURS_TARGET_PROCESSOR=%1
 
-pushd Detours\src || exit /b 1
+pushd Detours || exit /b 1
+
+rem Apply the EHCont compatibility patch
+git apply ..\patches\detours_ehcont.patch || exit /b 1
+
+pushd src || exit /b 1
 nmake || exit /b 1
+popd
+
+git reset --hard || exit /b 1
 popd
